87c65908d102c0ce9e7cb851339d8c4f49f825a3
squirrelcoding <rosilescarlos28@gmail.com>
2024-04-21 21:22:34 -0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { NextFont } from "next/dist/compiled/@next/font";
-import { Inter } from "next/font/google";
+import RootLayout from "../app/layout";
 
-const inter: NextFont = Inter({ subsets: ["latin"] });
-
-export default function ({ children }: Readonly<{ children: React.ReactNode }>) {
-	return <>
-		<body className={inter.className}>
-			<header className="auto-margins">
-				<h1 className="auto-margins">Navigation</h1>
-				<img src="./favicon.ico" alt="[Website icon.]" id="badge" />
-			</header>
-			<main className="auto-margins">{children}</main>
-		</body>
-	</>;
+export default function TestLayout() {
+  return <>
+      <p>Lorem ipsum</p>
+  </>
 }
